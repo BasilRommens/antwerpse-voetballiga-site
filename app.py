dd767c09bcef5fbe395bc76dc427248d83ee5ded
Basil Rommens <basil.rommens@student.uantwerpen.be>
2020-12-22 12:34:39 +0100

    Created best_of_division

--- app.py
+++ app.py
@@ -7,13 +7,21 @@ app = Flask(__name__)
 def login():
     return render_template('login.html')
 
+
 @app.route('/leagueTable')
 def league_table():
     return render_template('league_table.html')
 
+
 @app.route('/fixtures')
 def fixtures():
     return render_template('fixtures.html')
+
+
+@app.route('/bestOfDivision')
+def best_of_division():
+    return render_template('best_of_division.html')
+
 
 if __name__ == '__main__':
     app.run()
