ef4a7e507f5b754149bcac310878234c9c62a674
Basil Rommens <basil.rommens@student.uantwerpen.be>
2021-01-05 20:08:43 +0100

    added all the blueprints to the services list

--- testdriven-app/services/users/project/__init__.py
+++ testdriven-app/services/users/project/__init__.py
@@ -7,6 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 # Instantiate the database
 db = SQLAlchemy()
 
+
 def create_app(script_info=None):
     # instantiate the app
     app = Flask(__name__)
@@ -19,6 +20,30 @@ def create_app(script_info=None):
     db.init_app(app)
 
     # register blueprints
+    from project.api.admins import admin_blueprint
+    app.register_blueprint(admin_blueprint)
+
+    from project.api.clubs import club_blueprint
+    app.register_blueprint(club_blueprint)
+
+    from project.api.divisions import division_blueprint
+    app.register_blueprint(division_blueprint)
+
+    from project.api.matches import match_blueprint
+    app.register_blueprint(match_blueprint)
+
+    from project.api.models import model_blueprint
+    app.register_blueprint(model_blueprint)
+
+    from project.api.models import model_blueprint
+    app.register_blueprint(model_blueprint)
+
+    from project.api.seasons import season_blueprint
+    app.register_blueprint(season_blueprint)
+
+    from project.api.teams import team_blueprint
+    app.register_blueprint(team_blueprint)
+
     from project.api.users import users_blueprint
     app.register_blueprint(users_blueprint)
 
