70a5f6e9c829d910eeadd2833d2f09885acecc2b
Basil Rommens <basil.rommens@student.uantwerpen.be>
2021-01-05 21:10:03 +0100

    Created db api for users

--- testdriven-app/services/users/project/api/users.py
+++ testdriven-app/services/users/project/api/users.py
@@ -14,12 +14,13 @@ def add_user():
     password = post_data.get('password')
     clubID = post_data.get('clubID')
     try:
-        user = User.query.filter_by(username=username).first()
+        user = User.query.filter_by(email=email).first()
         if not user:
-            db.session.add(User(username=username, email=email))
+            db.session.add(User(username=username, email=email,
+                                password=password, clubID=clubID))
             db.session.commit()
             response_object['status'] = 'success'
-            response_object['message'] = f'{email} was added!'
+            response_object['message'] = f'{username} was added!'
             return jsonify(response_object), 201
         else:
             response_object['message'] = 'Sorry. That email already exists.'
@@ -29,7 +30,55 @@ def add_user():
         return jsonify(response_object), 400
 
 
-@users_blueprint.route('/users/<user_id>', methods=['GET'])
+@user_blueprint.route('/db/delete_user/<user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    post_data = request.get_json()
+    response_object = {'status': 'fail', 'message': 'Invalid payload.'}
+    try:
+        # Check for user existence
+        user = User.query.filter_by(ID=user_id).first()
+        if not user_user:
+            response_object['message'] = 'Sorry. Can\'t delete user'
+            return jsonify(response_object), 400
+        else:
+            user.delete()
+            db.session.commit()
+            return jsonify(response_object), 400
+    except exc.IntegrityError as e:
+        db.session.rollback()
+        return jsonify(response_object), 400
+
+
+@user_blueprint.route('/db/update_user', methods=['UPDATE'])
+def update_user():
+    post_data = request.get_json()
+    response_object = {'status': 'fail', 'message': 'Invalid payload.'}
+    if not post_data:
+        return jsonify(response_object), 400
+    user_id = post_data.get('ID')
+    username = post_data.get('username')
+    email = post_data.get('email')
+    password = post_data.get('password')
+    clubID = post_data.get('clubID')
+    try:
+        # Check for user existence
+        user = User.query.filter_by(ID=user_id).first()
+        if not user:
+            response_object['message'] = 'Sorry. Can\'t update user'
+            return jsonify(response_object), 400
+        else:
+            user.update({User.username: username, User.email: email, User.password: password,
+                         User.clubID: clubID})
+            db.session.commit()
+            response_object['status'] = 'success'
+            response_object['message'] = f'Updated user {username}'
+            return jsonify(response_object), 200
+    except exc.IntegrityError as e:
+        db.session.rollback()
+        return jsonify(response_object), 400
+
+
+@users_blueprint.route('/db/users/<user_id>', methods=['GET'])
 def get_single_user(user_id):
     """Get single user details"""
     response_object = {
@@ -47,7 +96,8 @@ def get_single_user(user_id):
                     'id': user.id,
                     'username': user.username,
                     'email': user.email,
-                    'active': user.active
+                    'password': user.password,
+                    'clubID': user.clubID
                 }
             }
             return jsonify(response_object), 200
