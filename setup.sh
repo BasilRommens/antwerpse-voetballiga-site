c5c31f81a989a147bb01d00a254010291488d193
Basil Rommens <basil.rommens@student.uantwerpen.be>
2021-01-09 10:40:48 +0100

    Created the file to setup the whole project

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo docker-compose -f docker-compose-dev.yml up -d --build
+sudo docker-compose -f docker-compose-dev.yml run database python manage.py recreate-db
+sudo docker-compose -f docker-compose-dev.yml run database python manage.py seed-db
