e29a7c12b7f7b3f9de80c42e7eeca29a532c142c
Basil Rommens <basil.rommens@student.uantwerpen.be>
2021-01-10 18:08:14 +0100

    fixed setup script

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@
 sudo docker-compose -f docker-compose-dev.yml up -d --build
 sudo docker-compose -f docker-compose-dev.yml run database python manage.py recreate-db
 sudo docker-compose -f docker-compose-dev.yml run database python manage.py seed-db
+xdg-open 'http://0.0.0.0:5004/'
